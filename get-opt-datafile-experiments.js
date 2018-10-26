0b11f88759ef0423c502336eb942ab4df4795feb
jordanmoritz <39655655+jordanmoritz@users.noreply.github.com>
2018-10-25 17:20:31 -0700

    Add files via upload

---
+++ get-opt-datafile-experiments.js
@@ -0,0 +1,12 @@
+var experimentKeys = [];
+
+var optFSJSON = $.ajax({
+        url: "https://cdn.optimizely.com/json/8511187875.json",
+        method: "GET"})
+    .done(function(responseText) {
+        window.dataFile = JSON.parse(responseText);
+        
+        for (i = 0; i < dataFile.experiments.length; i++) {
+            experimentKeys.push(dataFile.experiments[i].key);
+        };
+    });
