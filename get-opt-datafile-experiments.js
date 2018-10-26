dcd188e0e24449498b40bd2e7e7f41589ec289cf
jordanmoritz <39655655+jordanmoritz@users.noreply.github.com>
2018-10-25 17:22:23 -0700

    Update to version.

--- get-opt-datafile-experiments.js
+++ get-opt-datafile-experiments.js
@@ -3,10 +3,10 @@ var experimentKeys = [];
 var optFSJSON = $.ajax({
         url: "https://cdn.optimizely.com/json/8511187875.json",
         method: "GET"})
-    .done(function(responseText) {
-        window.dataFile = JSON.parse(responseText);
-        
-        for (i = 0; i < dataFile.experiments.length; i++) {
-            experimentKeys.push(dataFile.experiments[i].key);
-        };
+    	.done(function(responseText) {
+            window.dataFile = JSON.parse(responseText);
+
+            for (i = 0; i < dataFile.experiments.length; i++) {
+                experimentKeys.push(dataFile.experiments[i].key);
+            };
     });
